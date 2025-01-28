089d5fae421ccadbb445b3157449804bc8ce3ea0
Ilham Yudiatmoko <96367661+yudiatmoko@users.noreply.github.com>
2025-01-28 20:47:48 +0000

    Bug Create Transaction

--- services/TransactionService.js
+++ services/TransactionService.js
@@ -3,39 +3,103 @@ import ProductService from "./ProductService.js";
 import SessionService from "./SessionService.js";
 
 class TransactionService {
+  // async addTransaction(totalPayment, paymentMethod, note, sessionId, detail) {
+  //   await SessionService.getSessionById(sessionId);
+  //   const details = await Promise.all(
+  //     detail.map(async (item) => {
+  //       const existingProduct = await ProductService.getProductById(
+  //         item.productId
+  //       );
+  //       if (!existingProduct.status) {
+  //         throw new Error(`Product "${existingProduct.name}" is not available`);
+  //       }
+  //       if (existingProduct.stock !== null) {
+  //         if (existingProduct.stock === 0) {
+  //           throw new Error(
+  //             `Product "${existingProduct.name}" is out of stock.`
+  //           );
+  //         }
+  //         if (item.productQty > existingProduct.stock) {
+  //           throw new Error(
+  //             `Not enough stock for product "${existingProduct.name}". Available stock: ${existingProduct.stock}.`
+  //           );
+  //         }
+  //         const newStock = existingProduct.stock - item.productQty;
+  //         await ProductService.updateProductStock(existingProduct.id, newStock);
+  //       }
+  //       return {
+  //         productId: item.productId,
+  //         productName: existingProduct.name,
+  //         productQty: item.productQty,
+  //         productPrice: existingProduct.price,
+  //         totalPrice: item.productQty * existingProduct.price,
+  //       };
+  //     })
+  //   );
+  //   const totalPrice = details.reduce(
+  //     (total, item) => total + item.totalPrice,
+  //     0
+  //   );
+  //   if (totalPayment < totalPrice) {
+  //     throw new Error("Total payment is less than total price");
+  //   }
+  //   const totalCharge = totalPayment - totalPrice;
+  //   const transactionDate = new Date(Date.now());
+  //   const nextNumber = await TransactionModel.getNextTransactionNumber(
+  //     transactionDate
+  //   );
+  //   const transaction = {
+  //     number: nextNumber,
+  //     date: transactionDate,
+  //     totalPrice,
+  //     totalPayment,
+  //     totalCharge,
+  //     paymentMethod,
+  //     note: note || null,
+  //     sessionId: sessionId,
+  //   };
+  //   const newTransaction = await TransactionModel.addTransaction(transaction);
+  //   await TransactionModel.addTransactionDetail(newTransaction.id, details);
+  //   await SessionService.updateRevenue(sessionId, totalPrice);
+  //   const detailsWithoutTransactionId = details.map(
+  //     ({ transactionId, ...rest }) => rest
+  //   );
+  //   const result = {
+  //     ...newTransaction,
+  //     details: detailsWithoutTransactionId,
+  //   };
+  //   return result;
+  // }
+
   async addTransaction(totalPayment, paymentMethod, note, sessionId, detail) {
     await SessionService.getSessionById(sessionId);
-    const details = await Promise.all(
-      detail.map(async (item) => {
-        const existingProduct = await ProductService.getProductById(
-          item.productId
-        );
-        if (!existingProduct.status) {
-          throw new Error(`Product "${existingProduct.name}" is not available`);
-        }
-        if (existingProduct.stock !== null) {
-          if (existingProduct.stock === 0) {
-            throw new Error(
-              `Product "${existingProduct.name}" is out of stock.`
-            );
-          }
-          if (item.productQty > existingProduct.stock) {
-            throw new Error(
-              `Not enough stock for product "${existingProduct.name}". Available stock: ${existingProduct.stock}.`
-            );
-          }
-          const newStock = existingProduct.stock - item.productQty;
-          await ProductService.updateProductStock(existingProduct.id, newStock);
-        }
-        return {
-          productId: item.productId,
-          productName: existingProduct.name,
-          productQty: item.productQty,
-          productPrice: existingProduct.price,
-          totalPrice: item.productQty * existingProduct.price,
-        };
-      })
+    const productIds = detail.map((item) => item.productId);
+    const existingProducts = await TransactionModel.getProductsByIds(
+      productIds
     );
+    const details = detail.map(async (item) => {
+      const product = existingProducts.find((p) => p.id == item.productId);
+      if (!product) {
+        throw new Error(`Product "${item.productId}" does not exist.`);
+      }
+
+      if (!product.status) {
+        throw new Error(`Product "${product.name}" is not available.`);
+      }
+
+      if (product.stock !== null && product.stock < item.productQty) {
+        throw new Error(
+          `Not enough stock for product "${product.name}". Available: ${product.stock}.`
+        );
+      }
+      return {
+        productId: item.productId,
+        productName: existingProduct.name,
+        productQty: item.productQty,
+        productPrice: existingProduct.price,
+        totalPrice: item.productQty * existingProduct.price,
+      };
+    });
     const totalPrice = details.reduce(
       (total, item) => total + item.totalPrice,
       0
@@ -59,7 +123,19 @@ class TransactionService {
       sessionId: sessionId,
     };
     const newTransaction = await TransactionModel.addTransaction(transaction);
-    await TransactionModel.addTransactionDetail(newTransaction.id, details);
+    const transactionDetails = details.map((detail) => ({
+      transactionId: newTransaction.id,
+      ...detail,
+    }));
+    await TransactionModel.addTransactionDetail(
+      newTransaction.id,
+      transactionDetails
+    );
+    await Promise.all(
+      details.map((detail) =>
+        ProductService.updateProductStock(detail.productId, detail.productQty)
+      )
+    );
     await SessionService.updateRevenue(sessionId, totalPrice);
     const detailsWithoutTransactionId = details.map(
       ({ transactionId, ...rest }) => rest
